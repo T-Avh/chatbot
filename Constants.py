1d089c1830aac79e0c4d5bcd9ed66c3f407aeeb7
T-Avh <134845426+T-Avh@users.noreply.github.com>
2023-06-08 23:25:13 +0700

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,8 +1,8 @@
 import openai
 
 # OpenAI-API (get on OpenAI - must be of an USA account)
-openai.api_key = 'HIDDEN'
+openai.api_key = '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'
 
 # Token for fanpage and verify it
-_PAGE_ACCESS_TOKEN = 'HIDDEN'
-_VERIFY_TOKEN = 'HIDDEN'
+_PAGE_ACCESS_TOKEN = '110444828739720'
+_VERIFY_TOKEN = 'EAAIhFzetxp8BAILkt55lNS7F9FMgsPfqZAZCNmXW8oll2oKMGS4ryp8dgwmtZCkSOJxFqq5cuC9vr3ZAvnMDtdVcY19g4LQ6kiE9sv6ZAnfyBK6wBZBSjoWSTj6vigPIgDb884Rl8vzjK0PI4yOVfZCjY1UNREwJp4ZC7HgJ9Nx4SgLSIPCHz026'
